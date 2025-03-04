e25ce0272b4420e3c7082b7182f7e184472134d3
summy-hub <lateef.sumayyah2009@gmail.com>
2025-03-04 16:02:18 +0100

    first commit

--- Program.cs
+++ Program.cs
@@ -1,1 +1,12 @@
 ﻿
+Console.WriteLine("Enter a number");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter another number");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter another number");
+int c = int.Parse(Console.ReadLine());
+Console.WriteLine(ThreeSumming(a , b , c));
+static int ThreeSumming(int a, int b, int c)
+{
+    return a + b + c;
+}
